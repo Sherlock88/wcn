ccf4c8add7abfaf2ed9827b4f4f7e5b2bcb37d16
Its.Dipanjan.Das@gmail.com <Its.Dipanjan.Das@gmail.com@66652e3a-e613-6557-878a-e179d7ca9f44>
2014-11-10 08:44:15 +0000

    MATLAB plot file uploaded

---
+++ Plot_MC_PC.m
@@ -0,0 +1,28 @@
+delimiter = ' ';
+formatSpec = '%f%f%[^\n\r]';
+
+filename = 'E:\Experiment\Code\Projects\WCN\Data\MACRO-Dist-600-2000-1';
+fileID = fopen(filename,'r');
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true,  'ReturnOnError', false);
+fclose(fileID);
+MC_X = dataArray{:, 1};
+MC_Y = dataArray{:, 2};
+
+filename = 'E:\Experiment\Code\Projects\WCN\Data\PICO-Dist-600-6-1';
+fileID = fopen(filename,'r');
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true,  'ReturnOnError', false);
+fclose(fileID);
+PC_X = dataArray{:, 1};
+PC_Y = dataArray{:, 2};
+
+filename = 'E:\Experiment\Code\Projects\WCN\Data\UE-Dist-600-2000-1';
+fileID = fopen(filename,'r');
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true,  'ReturnOnError', false);
+fclose(fileID);
+UE_X = dataArray{:, 1};
+UE_Y = dataArray{:, 2};
+
+xlabel('X Coordinate');
+ylabel('Y Coordinate');
+title('Simulated locations of Macrocells and Picocells');
+plot(MC_X, MC_Y, 'rh', PC_X, PC_Y, 'bo', UE_X, UE_Y, 'g*');
