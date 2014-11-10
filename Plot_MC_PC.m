8874f22779cf8e6c5c906cc1ad5753264f89c7cd
Its.Dipanjan.Das@gmail.com <Its.Dipanjan.Das@gmail.com@66652e3a-e613-6557-878a-e179d7ca9f44>
2014-11-10 16:35:38 +0000

    Picocell generation

--- Plot_MC_PC.m
+++ Plot_MC_PC.m
@@ -15,14 +15,17 @@ fclose(fileID);
 PC_X = dataArray{:, 1};
 PC_Y = dataArray{:, 2};
 
+%{
 filename = 'E:\Experiment\Code\Projects\WCN\Data\UE-Dist-600-2000-1';
 fileID = fopen(filename,'r');
 dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true,  'ReturnOnError', false);
 fclose(fileID);
 UE_X = dataArray{:, 1};
 UE_Y = dataArray{:, 2};
+%}
 
 xlabel('X Coordinate');
 ylabel('Y Coordinate');
 title('Simulated locations of Macrocells and Picocells');
-plot(MC_X, MC_Y, 'rh', PC_X, PC_Y, 'bo', UE_X, UE_Y, 'g*');
+%plot(MC_X, MC_Y, 'rh', PC_X, PC_Y, 'bo', UE_X, UE_Y, 'g*');
+plot(MC_X, MC_Y, 'rh', PC_X, PC_Y, 'bo');
--- iitm/hpcn/generator/Point2DGenerator.java
+++ iitm/hpcn/generator/Point2DGenerator.java
@@ -1,27 +1,28 @@
 package iitm.hpcn.generator;
 
-import java.awt.geom.Point2D;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.LinkedHashSet;
+import java.awt.geom.Point2D;
+import java.io.PrintWriter;
+import java.util.Iterator;
 import java.util.Random;
+import java.util.Vector;
 import java.util.Set;
+import java.io.File;
 
 public class Point2DGenerator {
-	public static int N_SCENARIOS;
-	
-	private int radiusMacro, radiusPico, femtoCount, ueCount;
+	public static int N_SCENARIOS, totalPicoCount;
+	private int radiusMacro, radiusPico, picoCount, ueCount, scenarioCount;
 	private double hotSpotProb = 2.0/3;			//Non-Uniform Distribution
-	private static Set<Point2D> setMACRO;
-	private static Set<Point2D> setFEMTO;
-	private static Set<Point2D> setUE;
-	private int scenarioCount = 0;
+	private static Set<Point2D> setMACRO, setFEMTO, setUE;
+	private static Vector<Integer> picoPerMacro = new Vector<Integer>();
+	private final int MBSToPBS = 75, PBSToPBS = 40, MBSToUE = 35, PBSToUE = 10;
 	
-	public Point2DGenerator(int radiusM, int radiusP, int femtoCount, int ueCount, double ueDensity) {
+	public Point2DGenerator(int radiusM, int radiusP, int picoCount, int ueCount, double ueDensity) {
 		this.radiusMacro = radiusM;
 		this.radiusPico = radiusP;
-		this.femtoCount = femtoCount;
+		this.picoCount = picoCount;
+		totalPicoCount = 0;
 		this.ueCount = (int) Math.ceil(ueDensity *  Math.PI * (radiusM/1000.0) * (radiusM/1000.0));
 		System.out.println("UE count : " + this.ueCount);
 	}
@@ -37,12 +38,14 @@ public class Point2DGenerator {
 		femtoClusterGenerator(radiusM, radiusP, n_pico, n_ue, density, args[0], args[1], args[2], args[3]);
 	}
 	
-	public static void femtoClusterGenerator(int radiusM, int radiusP, int femtoCount, int ueCount, double ueDensity, String path, String ueFileName, String femtoFileName, String macroFileName) {
-		Point2DGenerator g = new Point2DGenerator(radiusM, radiusP, femtoCount, ueCount, ueDensity);
+	public static void femtoClusterGenerator(int radiusM, int radiusP, int picoCount, int ueCount, double ueDensity, String path, String ueFileName, String femtoFileName, String macroFileName) {
+		Point2DGenerator g = new Point2DGenerator(radiusM, radiusP, picoCount, ueCount, ueDensity);
 		for (int i = 1; i <= N_SCENARIOS; i++) {
 			setMACRO = new LinkedHashSet<Point2D>();
 			setFEMTO = new LinkedHashSet<Point2D>();
 			setUE = new LinkedHashSet<Point2D>();
+			totalPicoCount = 0;
+			picoPerMacro.clear();
 			g.generateClusterScenario();
 			g.saveToFile(path, ueFileName + "-" + i, femtoFileName + "-" + i, macroFileName + "-" + i);
 		}
@@ -55,34 +58,54 @@ public class Point2DGenerator {
 		++scenarioCount;
 		System.out.println("---------------------------\nScenario instance : " + scenarioCount);
 
-		// Generate a MACROCell at the center and six MACROCells surrounding it
+		// Generate a Macrocell at the center and six Macrocells surrounding it
 		
-		// MACROCell at the center
+		// Macrocell at the center
 		setMACRO.add(new Point2D.Double(0, 0));
-		picoFixedGenerator(0, 0, radiusMacro, femtoCount);
+		picoFixedGenerator(0, 0, radiusMacro, picoCount);
 		ueGenerator(0, 0, 0);
 		
-		// Surrounding MACROCells
+		// Surrounding Macrocells
 		for(i = 0; i < 6; i++)
 		{
 			theta = i * (2 * Math.PI / 6);
 			x = r * Math.cos(theta);
 			y = r * Math.sin(theta);
 			setMACRO.add(new Point2D.Double(x, y));
-			picoFixedGenerator(x, y, radiusMacro, femtoCount);
-			ueGenerator(x, y, i + 1);
+			picoFixedGenerator(x, y, radiusMacro, picoCount);
+			ueGenerator(x, y, i);
 		}
 	}
 	
-	public void picoFixedGenerator(double x, double y, int radius, int picoCount)
+	public void picoFixedGenerator(double x, double y, int radiusMacro, int picoCount)
 	{
 		double xCo, yCo;
-		double distance = radius - (radius / 4);
+		double distance;
+		Random random = new Random();
+		picoCount = 2 + random.nextInt(picoCount - 2);
 		double angle = (2 * Math.PI) / picoCount;
+		totalPicoCount += picoCount;
+		picoPerMacro.addElement(new Integer(picoCount));
+		
+		genPico:
 		for(int i = 0; i < picoCount; i++)
 		{
+			// Generating Picocell at a distance >= MBSToPBS from the Macrocell it's attached to
+			distance = MBSToPBS + random.nextDouble() * (radiusMacro - MBSToPBS);
 			xCo = x + distance * Math.cos(i * angle);
 			yCo = y + distance * Math.sin(i * angle);
+			
+			// Generating Picocell at a distance >= PBSToPBS from another Picocell
+			Iterator<Point2D> itrPico = setFEMTO.iterator();
+			while(itrPico.hasNext())
+			{
+				Point2D curPico = itrPico.next();
+				if(Point2D.distance(xCo, yCo, curPico.getX(), curPico.getY()) < PBSToPBS)
+				{
+					i--;
+					continue genPico;
+				}
+			}
 			setFEMTO.add(new Point2D.Double(xCo, yCo));
 		}
 	}
@@ -102,6 +125,7 @@ public class Point2DGenerator {
 	public void ueGenerator(double xMacro, double yMacro, int macroIndex)
 	{
 		Random prob = new Random((long)xMacro + (long)yMacro);
+		Integer intPico;
 		int hotspotUECount = 0, uniformUECount = 0, i;
 		Point2D[] arrFEMTO = setFEMTO.toArray(new Point2D[setFEMTO.size()]); 
 		
@@ -116,9 +140,10 @@ public class Point2DGenerator {
 		System.out.println("Hotspot / Uniform : " + hotspotUECount + " / " + uniformUECount);
 		getPoints(xMacro, yMacro, radiusMacro, uniformUECount, 0, 0);
 		
-		macroIndex *= 6;
-		for(i = macroIndex; i < macroIndex + 6; i++)
-			getPoints(arrFEMTO[i].getX(), arrFEMTO[i].getY(), radiusPico, hotspotUECount / femtoCount, 0, 0);
+		intPico = (Integer) picoPerMacro.get(macroIndex);
+		System.out.println("Pico/Macro: " + intPico);
+		for(i = totalPicoCount - intPico.intValue(); i < totalPicoCount; i++)
+			getPoints(arrFEMTO[i].getX(), arrFEMTO[i].getY(), radiusPico, hotspotUECount / intPico, 0, 0);
 	}
 	
 	/*
